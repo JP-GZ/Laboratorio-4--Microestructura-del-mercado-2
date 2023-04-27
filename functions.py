8628fa08160e42a12e50187f768073941154f76e
anadurrmuller <70038176+anadurrmuller@users.noreply.github.com>
2023-04-27 13:14:23 -0600

    cambios

--- functions.py
+++ functions.py
@@ -3,25 +3,23 @@ import numpy as np
 import pandas as pd
 
 
-def roll_spread_df(orderbook):  # Puede cambiar 
-    orderbook = orderbook.reset_index()
+def roll_spread_df(orderbook):
+    t0 = orderbook.loc[: ,"datetime"].values[0]
+    td = t0 + np.timedelta64(1, "m")
     
-    t1 = orderbook.loc[:, "timestamp"].values[-1]
-    t0 = t1 - datetime.timedelta(seconds=60)
-    n = sum(orderbook["timestamp"] <= t0)
-    window = len(orderbook) - n
+    n = sum(orderbook["datetime"] <= td)
 
-    pt1 = orderbook.loc[:, "close"]
+    pt = orderbook.loc[:, "mid_price"]
     
     effective_roll = []
     
-    for i in range(len(pt1)):
-        effective_roll.append(np.abs(np.cov(pt1[i:window + i], pt1[window+i:window*2+i]))**0.5)
+    for i in range(2*n, len(pt)):
+        pt0 = pt[i-2*n:i-n]
+        pt1 = pt[i-n:i]
+        effective_roll.append(2 * np.abs(np.cov(pt0, pt1)[0][1])**0.5)
     
-    orderbook = orderbook.iloc[n:, :]
-    orderbook["effective spread"] = effective_roll
-    return orderbook.loc[:, ["close", "spread", "effective spread"]]  # Puede cambiar dependiendo el nombre la columna
     
-
-
-orderbook_df = pd.read_json("...")  # Pendiente por datos
+    orderbook = orderbook.iloc[2*n:, :]
+    orderbook["effective_spread"] = effective_roll
+    return orderbook.loc[:, ["datetime", "mid_price", "spread", "effective_spread"]].set_index("datetime")
+    
